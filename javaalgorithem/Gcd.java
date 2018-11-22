5d8dc4ca0518519e0d43775cbff8c3df21d1a78e
kdrj <43085939+kdrj@users.noreply.github.com>
2018-11-22 13:14:59 +0800

    Update Gcd.java

--- javaalgorithem/Gcd.java
+++ javaalgorithem/Gcd.java
@@ -1,5 +1,10 @@
 package javaalgorithem;
-
+/**
+*--------------欧几里得算法---------------
+*(计算两个非负整数p和q的最大公约数：
+*若q是0,则最大公约数为p。否则，将p除以q得到的
+*余数r,p和q的最大公约数即为q和r的最大公约数)
+/*
 public class Gcd{
     public static void main(String[] args){
         int o=gcd(10,6);
